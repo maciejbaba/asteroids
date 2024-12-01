623359eab90ee42d692bb5b379d40fc08d23644c
Maciej Baba <maciek2baba2@gmail.com>
2024-12-01 22:19:52 +0100

    add constants, init pygame, create window

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ def main():
 
     pygame.Surface.fill(screen, (0, 0, 0))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
